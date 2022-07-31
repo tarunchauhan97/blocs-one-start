e4f2a52378f3cc64d713cb3a12d962034f6e10df
kashif ahmad <kashif.ahamd@antino.io>
2022-07-31 10:27:48 +0530

    large application routes blocs

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,32 +6,12 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:bloc_test/bloc_test.dart';
-import 'package:blocs_one/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:blocs_one/main.dart';
 
 void main() {
-  group('CounterCubit', () {
-    CounterCubit? counterCubit;
-
-    setUp(() {
-      counterCubit = CounterCubit();
-    });
-    tearDown(() {
-      counterCubit!.close();
-    });
-    test('initial state of CounterCubit is CounterState(counterValue:0)', () {
-      expect(counterCubit!.state, CounterState(counterValue: 0));
-    });
-
-    // blocTest(
-    //     'the CounterCubit should emit a CounterState(counterValue:1, wasIncremented:true) when the increment function is called',
-    //     build: () => counterCubit,
-    //     act: (cubit) => cubit.increment(),
-    //     expect: [CounterState(counterValue: 1, wasIncremented: true)]);
-  });
 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
